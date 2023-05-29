8e2da3ac973a1778cdb38de2a163727b94248ab0
Saysherief <suhairdama@gmail.com>
2023-05-29 14:54:06 -0700

    removed \

--- mod.c
+++ mod.c
@@ -16,7 +16,7 @@ void mod(stack_t **stack, unsigned int line_number)
 	}
 	if (((*stack)->n) == 0)
 	{
-		fprintf(stderr, "L%u\: division by zero\n", line_number);
+		fprintf(stderr, "L%u: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	((*stack)->next)->n %= (*stack)->n;
